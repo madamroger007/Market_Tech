d15bb2e205745c7cdf7e06f3b9d951e40bf94e58
madam007 <adamsetiadijr07@gmail.com>
2023-10-15 08:04:50 +0700

    Feat: Menambahkan fitur Menambahkan komponen form memperbaiki kode di file product form mengganti nama file menjadi camelcase

---
+++ components/ProductForm.js
@@ -0,0 +1,100 @@
+import Layout from "@/components/Layout";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+export default function ProductForm({
+  _id,
+  title: existTitle,
+  description: existDescription,
+  price: existPrice,
+  images,
+}) {
+  const [title, setTitle] = useState(existTitle || "");
+  const [description, setDescription] = useState(existDescription || "");
+  const [price, setPrice] = useState(existPrice || "");
+  const [goToProducts, setGoToProducts] = useState(false);
+  const router = useRouter();
+
+  async function saveProduct(ev) {
+    ev.preventDefault();
+    const data = { title, description, price };
+    if (_id) {
+      // update
+      await axios.put("/api/products", { ...data, _id });
+    } else {
+      // create
+      await axios.post("/api/products", data);
+    }
+    setGoToProducts(true);
+  }
+  if (goToProducts) {
+    router.push("/products");
+  }
+
+  async function uploadImage(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      const res = await axios.post("/api/upload", data, {
+        headers: {'Content-Type':"multiparty/form-data"},
+      });
+      console.log(res.data);
+    }
+  }
+
+  return (
+    <form onSubmit={saveProduct}>
+      <label>Product name</label>
+      <input
+        type="text"
+        placeholder="product name"
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+
+      <label>Image</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 border text-center flex flex-col items-center justify-center text-sm text-gray-500 rounded-md bg-gray-200 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImage} className="hidden" />
+        </label>
+        {!images?.length && <div>No photos in this product</div>}
+      </div>
+
+      <label>Description</label>
+      <textarea
+        placeholder="description"
+        value={description}
+        onChange={(ev) => setDescription(ev.target.value)}
+      />
+      <label>Price (in USD)</label>
+      <input
+        type="number"
+        placeholder="price"
+        value={price}
+        onChange={(ev) => setPrice(ev.target.value)}
+      />
+      <button type="submit" className="btn-element">
+        Save
+      </button>
+    </form>
+  );
+}
